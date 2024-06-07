da3bd3001d5e3c66b2b3b544649a93caa982a766
rosemolt <rosemolthomas02@gmail.com>
2024-06-07 04:13:12 -0700

    Project created

---
+++ src/components/ViewStudent.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewStudent = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..." />
+                                        <div class="card-body">
+                                            <h5 class="card-title">Sofia</h5>
+                                            <a href="#" class="btn btn-primary">View Details</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Sofia</h5>
+                                            <a href="#" class="btn btn-primary">View Details</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Sofia</h5>
+                                            <a href="#" class="btn btn-primary">View Details</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Sofia</h5>
+                                            <a href="#" class="btn btn-primary">View Details</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Sofia</h5>
+                                            <a href="#" class="btn btn-primary">View Details</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Sofia</h5>
+                                            <a href="#" class="btn btn-primary">View Details</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Sofia</h5>
+                                            <a href="#" class="btn btn-primary">View Details</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Sofia</h5>
+                                            <a href="#" class="btn btn-primary">View Details</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Sofia</h5>
+                                            <a href="#" class="btn btn-primary">View Details</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Sofia</h5>
+                                            <a href="#" class="btn btn-primary">View Details</a>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewStudent
