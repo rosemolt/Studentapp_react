1d25f7f51f5591a85abc057a52565928334fcca5
rosemolt <rosemolthomas02@gmail.com>
2024-06-09 10:14:49 -0700

    Project created

--- src/components/SearchStudent.jsx
+++ src/components/SearchStudent.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const SearchStudent = () => {
+    const [data,setData]=useState(
+        {
+            "name":""
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
         <Navbar/>
@@ -11,10 +22,10 @@ const SearchStudent = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">Student Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success">Search</button>
+                            <button className="btn btn-success" onClick={readValue}>Search</button>
                         </div>
                     </div>
                 </div>
--- src/components/ViewStudent.jsx
+++ src/components/ViewStudent.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const ViewStudent = () => {
+    const [data,changeData]=useState(
+        [
+            {"name":"Rose","admNo":101,"rollNo":1},
+            {"name":"Surya","admNo":102,"rollNo":2},
+            {"name":"Raifa","admNo":103,"rollNo":3}
+        ]
+    )
     return (
         <div>
             <Navbar/>
@@ -9,96 +16,22 @@ const ViewStudent = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..." />
-                                        <div class="card-body">
-                                            <h5 class="card-title">Sofia</h5>
-                                            <a href="#" class="btn btn-primary">View Details</a>
+                            {
+                                data.map(
+                                    (value,index) => {
+                                        return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                        <div class="card">
+                                            <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..." />
+                                                <div class="card-body">
+                                                    <h5 class="card-title">{value.name}</h5>
+                                                    <p class="card-text">Admission No:{value.admNo}</p>
+                                                    <a href="#" class="btn btn-primary">View Details</a>
+                                                </div>
                                         </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Sofia</h5>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Sofia</h5>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Sofia</h5>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Sofia</h5>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Sofia</h5>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Sofia</h5>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Sofia</h5>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Sofia</h5>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://bizimages.withfloats.com/tile/64e38a7942b3f80001deb2b1.jpg" class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Sofia</h5>
-                                            <a href="#" class="btn btn-primary">View Details</a>
-                                        </div>
-                                </div>
-                            </div>
+                                    </div>
+                                    }
+                                )
+                            }
                         </div>
                     </div>
                 </div>
